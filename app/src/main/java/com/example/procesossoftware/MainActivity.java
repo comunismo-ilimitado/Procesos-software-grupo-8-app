97410ac73a4d07de51368639076a7c892579e52d
Jorge Vega Arias <jorgevegarias1@gmail.com>
2023-10-13 00:23:58 +0200

    Revert "Scrum 14 definir requisitos"

--- app/src/main/java/com/example/procesossoftware/MainActivity.java
+++ app/src/main/java/com/example/procesossoftware/MainActivity.java
@@ -6,8 +6,6 @@ import android.view.MenuItem;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -16,62 +14,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        setScreen1();
-
-
-
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
         bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
-            FragmentTransaction fragmentTransaction2 = getSupportFragmentManager().beginTransaction(); // Creamos una nueva transacción para cada reemplazo
             if (item.getItemId() == R.id.action_screen1) {
-
-                setScreen1();
+                startActivity(new Intent(MainActivity.this, ScreenActivity1.class));
                 return true;
             } else if (item.getItemId() == R.id.action_screen2) {
-
-                setScreen2();
+                startActivity(new Intent(MainActivity.this, ScreenActivity2.class));
                 return true;
             } else if (item.getItemId() == R.id.action_screen3) {
-                setScreen3();
+                startActivity(new Intent(MainActivity.this, ScreenActivity3.class));
                 return true;
             }
-
             return false;
         });
-    }
-
-    private void setScreen2() {
-        Fragment_s2 fragment = new Fragment_s2();
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        fragmentManager.beginTransaction()
-                .replace(R.id.fragment_container,fragment)
-                .commit();
-    }
-
-    private void setScreen1() {
-        Fragment_s1 fragment = new Fragment_s1();
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        fragmentManager.beginTransaction()
-                .replace(R.id.fragment_container,fragment)
-                .commit();
-
-    }
-    private void setScreen3() {
-        Fragment_s3 fragment = new Fragment_s3();
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        fragmentManager.beginTransaction()
-                .replace(R.id.fragment_container,fragment)
-                .commit();
-
-    }
-    private void setScreen4() {
-        Fragment_s4 fragment = new Fragment_s4();
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        fragmentManager.beginTransaction()
-                .replace(R.id.fragment_container,fragment)
-                .commit();
-
     }
 
 }
---
+++ app/src/main/java/com/example/procesossoftware/ScreenActivity1.java
@@ -0,0 +1,14 @@
+package com.example.procesossoftware;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ScreenActivity1 extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_screen1);
+    }
+}
